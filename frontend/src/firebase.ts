ffdc9fc53ce18ece2149841a1cd3f993439cfe77
vargas <guilhermeinfvargas@gmail.com>
2026-03-11 23:23:19 -0300

    move firebase config to env vars

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCUIQ1NJKgbD5qrkdpwKd6PYCBz28DHDJc",
-  authDomain: "dca-guard-dev.firebaseapp.com",
-  projectId: "dca-guard-dev",
-  storageBucket: "dca-guard-dev.firebasestorage.app",
-  messagingSenderId: "328831244715",
-  appId: "1:328831244715:web:5ae80e9eddddfbeb501aee"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
